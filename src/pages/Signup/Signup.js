82cec1073c23522115d6c15928058db30abdce2e
vivek-guda <vivekreddy1000@gmail.com>
2022-04-16 23:08:36 +0530

    added login and signup pages

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -3,7 +3,33 @@ import "../Login/Login.css";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <main className="login-main">
+      <div className="login-container">
+        <h1 className="login-title">Signup</h1>
+        <label className="login-label">Email</label>
+        <input className="login-input" type="text" placeholder="Enter email" />
+        <label className="login-label">Password</label>
+        <input
+          className="login-input"
+          type="password"
+          placeholder="Enter password"
+        />
+        <label className="login-label">Confirm Password</label>
+        <input
+          className="login-input"
+          type="password"
+          placeholder="Confirm password"
+        />
+        <Link className="login-btn" to="/">
+          Fake Sign Up
+        </Link>
+        <p className="alternate-msg">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Signup;
